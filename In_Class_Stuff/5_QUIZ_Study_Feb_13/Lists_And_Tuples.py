df52b916b4f98bed027e1009559ae591971af327
Saif <sshaikh4@ggc.edu>
2023-02-13 22:42:38 -0500

    additional changes from PC

---
+++ In_Class_Stuff/5_QUIZ_Study_Feb_13/Lists_And_Tuples.py
@@ -0,0 +1,87 @@
+# Lists ----------------------------------------------------------------------------------------------------------------
+# Lists have []
+# lists can be changed
+
+integer_list = [1 ,2 ,3]
+heterogenous_list = ["String", 0.1, True]
+list_of_lists = [integer_list, heterogenous_list, []]
+
+list_length = len(integer_list)     # equals 3
+list_sum    = sum(integer_list)     # equals 6
+
+# position in list
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+zero = x[0]          # equals 0, lists are 0-indexed
+one = x[1]           # equals 1
+nine = x[-1]         # equals 9, 'Pythonic' for last element
+eight = x[-2]        # equals 8, 'Pythonic' for next-to-last element
+x[0] = -1            # now x is [-1, 1, 2, 3, ..., 9]
+
+# slicing in list
+first_three = x[:3]                 # [-1, 1, 2]
+three_to_end = x[3:]                # [3, 4, ..., 9]
+one_to_four = x[1:5]                # [1, 2, 3, 4]
+last_three = x[-3:]                 # [7, 8, 9]
+without_first_and_last = x[1:-1]    # [1, 2, ..., 8]
+copy_of_x = x[:]                    # [-1, 1, 2, ..., 9]
+
+# special slicing
+every_third = x[::3]                 # [-1, 3, 6, 9]
+five_to_three = x[5:2:-1]            # [5, 4, 3]
+
+# using in for lists
+1 in [1, 2, 3]    # True
+0 in [1, 2, 3]    # False
+
+# extend list
+x = [1, 2, 3]
+x.extend([4, 5, 6])     # x is now [1, 2, 3, 4, 5, 6]
+
+# add list
+x = [1, 2, 3]
+y = x + [4, 5, 6]       # y is [1, 2, 3, 4, 5, 6]; x is unchanged
+
+# append list
+x = [1, 2, 3]
+x.append(0)      # x is now [1, 2, 3, 0]
+y = x[-1]        # equals 0
+z = len(x)       # equals 4
+
+# unpack lists
+x, y = [1, 2]    # now x is 1, y is 2
+
+#value to throw away
+_, y = [1, 2]    # now y == 2, didn't care about the first element
+
+
+# Tuples ---------------------------------------------------------------------------------------------------------------
+# tuples have ()
+# tuples can not be changed
+
+my_list = [1, 2]
+my_tuple = (1, 2)
+other_tuple = 3, 4
+my_list[1] = 3      # my_list is now [1, 3]
+
+try:
+    my_tuple[1] = 3
+except TypeError:
+    print("cannot modify a tuple")
+
+# Tuples are a convenient way to return multiple values from functions:
+def sum_and_product(x, y):
+    return (x + y), (x * y)
+
+sp = sum_and_product(2, 3)     # sp is (5, 6)
+s, p = sum_and_product(5, 10)  # s is 15, p is 50
+
+# multiple assignments
+x, y = 1, 2     # now x is 1, y is 2
+x, y = y, x     # Pythonic way to swap variables; now x is 2, y is 1
+
+
+
+
+
+
